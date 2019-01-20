548f8ac0a6622fabcdb2c511095a12c33c53fcb1
Ben Cao <benjamin.cao88@gmail.com>
2019-01-19 21:41:56 -0500

    S11L117

---
+++ S11L117/README.md
@@ -0,0 +1,4 @@
+# Section 11: Javascript, Lecture 117: Exercise: Build Facebook 2
+A continuation of Lecture 114's exercise using Javascript.
+
+[The Complete Web Developer in 2019: Zero to Mastery by Andrei Neagoie](https://www.udemy.com/the-complete-web-developer-in-2018/)
---
+++ S11L117/script.js
@@ -0,0 +1,52 @@
+var database = [
+  {
+    username: "andrei",
+    password: "supersecret"
+  },
+  {
+    username: "sally",
+    password: "123"
+  },
+  {
+    username: "ingrid",
+    password: "777"
+  }
+];
+
+var newsFeed = [
+  {
+    username: "Bobby",
+    timeline: "So tired from all that learning."
+  },
+  {
+    username: "Sally"
+    timeline: "Javascript is cooooo coool!"
+  },
+  {
+    username: "Mitch",
+    timeline: "Javascript is preeetyy cool!"
+  }
+];
+
+function isUserValid(username, password) {
+  for (var i = 0; i < database.length; i++) {
+    if(database[i].username === username && database[i].password === password) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function signIn(username, password) {
+  if (isUserValid(username, password)) {
+    console.log(newsFeed);
+  }
+  else {
+    console.log("Sorry, wrong username and password!");
+  }
+}
+
+var userNamePrompt = prompt("What\'s your username?");
+var passwordPrompt = prompt("What\'s your password?");
+
+signIn(userNamePrompt, passwordPrompt);
